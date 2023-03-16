e2d61af4de5e468f9ce5ae5bb92e2b1d98c1ec39
dillonl <dillonl49@163.com>
2023-03-17 00:09:32 +0800

    feat:完成部分页面

--- lib/local_data/home.dart
+++ lib/local_data/home.dart
@@ -30,6 +30,205 @@ List navList = [
     "image": "assets/images/nav/nav_6.png"
   }
 ];
+Map infoData = {
+  "id": 841039246025293935,
+  "userId": 8741451,
+  "familyId": 0,
+  "weight": 87.1,
+  "physicalBodyType": "肥胖",
+  "time": "03月10日 19:49",
+  "daysBefore": "6天前",
+  "bmiType": 1,
+  "bmi": 26.6,
+  "bmiUpOrDown": 1,
+  "fatType": 2,
+  "fat": 0.31,
+  "fatUpOrDown": -1,
+  "fatWeightType": 3,
+  "fatWeight": 26.65,
+  "fatWeightUpOrDown": -1,
+  "weightType": 17,
+  "weightUpOrDown": 1,
+  "weightChange": 0.95,
+  "lastWeightTime": "2022.09.12"};
+List compareVos= [
+    {
+      "type": 2,
+      "name": "BMI",
+      "unit": "",
+      "num": "26.6",
+      "level": "肥胖",
+      "levelColor": "#fa4f2f",
+      "upOrDown": 1,
+      "changeValue": "0.3",
+      "sort": 1
+    },
+    {
+      "type": 3,
+      "name": "体脂率",
+      "unit": "%",
+      "num": "30.6",
+      "level": "超高",
+      "levelColor": "#fa4f2f",
+      "upOrDown": -1,
+      "changeValue": "3.4",
+      "sort": 2
+    },
+    {
+      "type": 4,
+      "name": "脂肪重量",
+      "unit": "KG",
+      "num": "26.65",
+      "level": "超高",
+      "levelColor": "#fa4f2f",
+      "upOrDown": -1,
+      "changeValue": "2.64",
+      "sort": 3
+    },
+    {
+      "type": 15,
+      "name": "去脂重量",
+      "unit": "KG",
+      "num": "60.4",
+      "level": "",
+      "levelColor": "#6CE657",
+      "upOrDown": 1,
+      "changeValue": "3.5",
+      "sort": 4
+    },
+    {
+      "type": 1,
+      "name": "肥胖度",
+      "unit": "%",
+      "num": "23.2",
+      "level": "肥胖",
+      "levelColor": "#fa4f2f",
+      "upOrDown": 1,
+      "changeValue": "1.4",
+      "sort": 5
+    },
+    {
+      "type": 5,
+      "name": "骨骼肌率",
+      "unit": "%",
+      "num": "31.0",
+      "level": "优",
+      "levelColor": "#1ab733",
+      "upOrDown": -1,
+      "changeValue": "1.0",
+      "sort": 6
+    },
+    {
+      "type": 6,
+      "name": "骨骼肌重量",
+      "unit": "KG",
+      "num": "27.4",
+      "level": "优",
+      "levelColor": "#1ab733",
+      "upOrDown": 1,
+      "changeValue": "0.1",
+      "sort": 7
+    },
+    {
+      "type": 7,
+      "name": "肌肉率",
+      "unit": "%",
+      "num": "64.0",
+      "level": "优",
+      "levelColor": "#1ab733",
+      "upOrDown": 1,
+      "changeValue": "3.0",
+      "sort": 8
+    },
+    {
+      "type": 8,
+      "name": "肌肉重量",
+      "unit": "KG",
+      "num": "55.7",
+      "level": "优",
+      "levelColor": "#1ab733",
+      "upOrDown": 1,
+      "changeValue": "3.1",
+      "sort": 9
+    },
+    {
+      "type": 9,
+      "name": "内脏脂肪",
+      "unit": "级",
+      "num": "8",
+      "level": "标准",
+      "levelColor": "#57cf6a",
+      "upOrDown": -1,
+      "changeValue": "1",
+      "sort": 10
+    },
+    {
+      "type": 10,
+      "name": "水分",
+      "unit": "%",
+      "num": "53.0",
+      "level": "优",
+      "levelColor": "#1ab733",
+      "upOrDown": -1,
+      "changeValue": "1.0",
+      "sort": 11
+    },
+    {
+      "type": 11,
+      "name": "水含量",
+      "unit": "KG",
+      "num": "46.2",
+      "level": "优",
+      "levelColor": "#1ab733",
+      "upOrDown": -1,
+      "changeValue": "0.3",
+      "sort": 12
+    },
+    {
+      "type": 12,
+      "name": "基础代谢",
+      "unit": "kcal",
+      "num": "1994.3",
+      "level": "优",
+      "levelColor": "#57cf6a",
+      "upOrDown": 1,
+      "changeValue": "13.0",
+      "sort": 13
+    },
+    {
+      "type": 13,
+      "name": "骨量",
+      "unit": "KG",
+      "num": "4.3",
+      "level": "偏高",
+      "levelColor": "#f8ab3e",
+      "upOrDown": 0,
+      "changeValue": "无变化",
+      "sort": 14
+    },
+    {
+      "type": 14,
+      "name": "蛋白质",
+      "unit": "%",
+      "num": "14.0",
+      "level": "不足",
+      "levelColor": "#0eb6ed",
+      "upOrDown": -1,
+      "changeValue": "0.3",
+      "sort": 15
+    },
+    {
+      "type": 16,
+      "name": "身体年龄",
+      "unit": "岁",
+      "num": "30",
+      "level": "偏大",
+      "levelColor": "#f8ab3e",
+      "upOrDown": 1,
+      "changeValue": "1",
+      "sort": 16
+    }
+  ];
 // 饮食记录 底部
 List mealsList = [
   {"id": 0, "icon": 0xe635, "title": "早餐"},
