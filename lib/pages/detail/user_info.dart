8b1194f306b11811d0052bcd56f7b93e92d2567c
dillonl <dillonl49@163.com>
2023-03-20 21:37:20 +0800

    fix:#创建用户信息页面

---
+++ lib/pages/detail/user_info.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+class UserInfo extends StatefulWidget {
+  const UserInfo({Key? key}) : super(key: key);
+
+  @override
+  _UserInfoState createState() => _UserInfoState();
+}
+
+class _UserInfoState extends State<UserInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
