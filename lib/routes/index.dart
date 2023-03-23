4590536142771d58598655142d8821604a7b4727
dillonl <dillonl49@163.com>
2023-03-23 21:48:09 +0800

    fix:#更改路由

--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -31,10 +31,10 @@ import 'package:sunset/pages/sign/bind_phone.dart'; // 更换绑定手机号
 import 'package:sunset/pages/Test.dart'; // 测试页面
 final Map<String, WidgetBuilder> routes = {
   // "/":(BuildContext context) => Test(),
-  // "/":(BuildContext context) => App(), // 说明：这里如果指定了页面那么在 main中 home: HomePage() 不能存在
+  "/":(BuildContext context) => App(), // 说明：这里如果指定了页面那么在 main中 home: HomePage() 不能存在
   "aboutApp": (BuildContext context) => AboutApp(),
   "myDevice": (BuildContext context) => MyDevice(),
-  "/": (BuildContext context) => BindDevice(),
+  "bindDevice": (BuildContext context) => BindDevice(),
   "balance": (BuildContext context) => Balance(),
   "themeSkin": (BuildContext context) => ThemeSkin(),
   "family": (BuildContext context) => Family(),
