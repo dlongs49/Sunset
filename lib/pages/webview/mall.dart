3430ca5bb5c5b1a88476fa783740cc86ade4f58d
dillonl <dillonl49@163.com>
2023-04-09 01:58:53 +0800

    fix:#优化h5内嵌进度条

--- lib/pages/webview/mall.dart
+++ lib/pages/webview/mall.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sunset/components/tabbar.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Mall extends StatefulWidget {
   const Mall({Key? key}) : super(key: key);
@@ -11,35 +12,33 @@ class Mall extends StatefulWidget {
 }
 
 class _MallState extends State<Mall> {
+  double progress = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: Colors.white,
-      child: Expanded(
-          child: Column(
-        children: [
-          CustomTabBar(title: "香豆商城", bgColor: null, fontColor: null),
-          Expanded(
-              child: (Container(
-                  color: Color(0xFFF6F7FB),
-                  // child:
-                  // WebviewScaffold(
-                  //     url: 'https://main.m.taobao.com/',
-                  //     withZoom: false,
-                  //     // 是否缩放
-                  //     withLocalStorage: true,
-                  //     withJavascript: true,
-                  //     hidden: false,
-                  //     //等待页面加载时显示其他小部件hiden配合initialChild使用
-                  //     initialChild: Center(
-                  //         child: CupertinoActivityIndicator(
-                  //       radius: 15.0,
-                  //       animating: true,
-                  //     )))
-              ))),
-        ],
-      )),
-    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            CustomTabBar(title: "香豆商城", bgColor: null, fontColor: null),
+            progress != 1.0
+                ? LinearProgressIndicator(
+                    value: progress,
+                    backgroundColor: Color(0xffffffff),
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(Color(0xff22d47e)),
+                  )
+                : Container(height: 4),
+            Expanded(
+                child: Container(
+                    color: Color(0xFFF6F7FB),
+                    child: WebView(
+                        initialUrl: "https://main.m.taobao.com/",
+                        javascriptMode: JavascriptMode.unrestricted,
+                        onProgress: (int gress) {
+                          progress = (gress / 100);
+                        }))),
+          ],
+        ));
   }
 }
