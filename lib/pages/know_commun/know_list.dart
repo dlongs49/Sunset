44733c2e4d494ade25564c5faaad3e361fb06a77
dillonl <dillonl49@163.com>
2023-04-16 00:19:12 +0800

    feat:增加知识社区列表页面

---
+++ lib/pages/know_commun/know_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class KnowList extends StatefulWidget {
+  const KnowList({Key? key}) : super(key: key);
+
+  @override
+  _KnowListState createState() => _KnowListState();
+}
+
+class _KnowListState extends State<KnowList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
