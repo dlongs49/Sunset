07641a7bfb56e22077230a12c66783f1f28608ed
dillonl <dillonl49@163.com>
2023-04-16 22:55:08 +0800

    feat:#增加文章详情webview页面

--- lib/pages/webview/know_detail.dart
+++ lib/pages/webview/know_detail.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sunset/components/tabbar.dart';
+import 'package:sunset/components/toast.dart';
+import 'package:sunset/utils/api/know_req.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class KnowDetail extends StatefulWidget {
@@ -14,6 +16,25 @@ class KnowDetail extends StatefulWidget {
 class _KnowDetailState extends State<KnowDetail> {
   final arguments;
   _KnowDetailState({this.arguments});
+  KnowReq knowReq = new KnowReq();
+  Map item = {};
+
+  // 收藏 & 取消收藏
+  void handleLike () async{
+    try {
+      Map res =
+      await knowReq.likeKnow({"id": 1});
+      print("知识精选>>${res["data"]}");
+      if (res["code"] == 200) {
+        item["islike"] = res["data"]["list"];
+        setState(() {});
+      }
+    } catch (e) {
+      print(e);
+      errToast();
+    }
+  }
+
   // H5 加载进度
   double progress = 0;
   @override
@@ -35,19 +56,34 @@ class _KnowDetailState extends State<KnowDetail> {
               color: Color(0xFFF6F7FB),
               child: WebView(
                   initialUrl:
-                  "https://main.m.taobao.com/",
+                  "https://www.taobao.com/",
                   javascriptMode: JavascriptMode.unrestricted,
                   onProgress: (int gress) {
                     progress = (gress / 100);
                     setState(() {});
                   }))),
           Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 36),
+            padding: EdgeInsets.only(top: 12),
+            height: 60,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
                   child: Row(
                     children: [
-                      Icon()
+                      Icon(IconData(0xe603,fontFamily: "sunfont"),size: 24,color: Color(0xff999999)),
+                      Text("收藏 9",style: TextStyle(color: Color(0xff999999),fontSize: 14))
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Icon(IconData(0xe6ba,fontFamily: "sunfont"),size: 24,color: Color(0xff999999)),
+                      Text("评论 9",style: TextStyle(color: Color(0xff999999),fontSize: 14))
                     ],
                   ),
                 )
---
+++ lib/utils/api/know_req.dart
@@ -0,0 +1,24 @@
+import 'package:sunset/utils/request.dart';
+
+Http http = new Http();
+
+class KnowReq {
+  // 知识精选
+  Future<Map> getKnow(params) async {
+    Map<String, dynamic> data = params;
+    return await http.get(
+        "/know/list", data);
+  }
+  // 文章详情
+  Future<Map> getKnowDetail(params) async {
+    Map<String, dynamic> data = params;
+    return await http.get(
+        "/know/detail", data);
+  }
+  // 收藏文章
+  Future<Map> likeKnow(params) async {
+    Map<String, dynamic> data = params;
+    return await http.post(
+        "/know/like", data);
+  }
+}
