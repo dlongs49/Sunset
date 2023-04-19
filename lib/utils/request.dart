445b2c8ae1a32823ac8980aca1902ef4a9fff1a6
dillonl <dillonl49@163.com>
2023-04-19 17:08:26 +0800

    fix:#修改token

--- lib/utils/request.dart
+++ lib/utils/request.dart
@@ -25,7 +25,7 @@ class Http {
       // 赋值请求头
       // options.headers["ms_token"] = ms_token;
       // options.headers["ms_token"] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiI4NDU0MTJGRi1DREVGLTQxNTgtODIyMS1CRjM5NkYwQzQwNEIiLCJzdGFtcCI6MTY4MTMwMzYzODI4NSwiZXhwIjoxNjgxNTE5NjM4fQ.joPKBfbDeFzvPK85LkqvVr1fxXad32pycvFFMv50II0";
-      options.headers["ms_token"] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiI4MjgxNjExRi00RkEwLTQyMzgtQkFCRC03MUY0NkVCMjVCODgiLCJzdGFtcCI6MTY4MTgyMzUxMjkxNCwiZXhwIjoxNjgyMDM5NTEyfQ.3fbldW5LEzc3fh9mYojXLCmURyGs8_72jsKwC2n1oI8";
+      options.headers["ms_token"] = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiI4MjgxNjExRi00RkEwLTQyMzgtQkFCRC03MUY0NkVCMjVCODgiLCJzdGFtcCI6MTY4MTg5NTA4NDA5MSwiZXhwIjoxNjgyMTExMDg0fQ.qgPpOsqcsPJRcXj1vqwWxdeBE-7Q32gatd4EFxh07vQ";
       print("开始请求：${options.baseUrl}");
       return handler.next(options);
     }, onResponse: (response, handler) async {
