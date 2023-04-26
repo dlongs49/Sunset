c49a75a69ea4f1d012809f5e567192824769e435
dillonl <dillonl49@163.com>
2023-04-26 22:36:24 +0800

    fix:#更新依赖

--- lib/pages/Test.dart
+++ lib/pages/Test.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_blue_elves/flutter_blue_elves.dart';
 
 class Test extends StatefulWidget {
   const Test({Key? key}) : super(key: key);
@@ -10,7 +11,37 @@ class Test extends StatefulWidget {
   _TestState createState() => _TestState();
 }
 
-class _TestState extends State<Test>{
+class _TestState extends State<Test> {
+  void initState() {
+    super.initState();
+    initBlue();
+  }
+
+  void initBlue() {
+    FlutterBlueElves.instance.startScan(1000).listen((scanItem) {
+      print(">>>${scanItem.id}--${scanItem.name}");
+      if (scanItem.id == "D7:AF:73:3B:A2:55") { // 10:96:1A:6C:B6:50
+        Device device = scanItem.connect(connectTimeout: 5000);
+        device.stateStream.listen((state) {
+          print("连接状态>>>>${state}");
+          if(state == DeviceState.connected){
+            FlutterBlueElves.instance.stopScan();
+            device.serviceDiscoveryStream.listen((serviceItem) {
+              if (serviceItem.serviceUuid == "89315c8f-8dae-4746-a270-1954dfcb6108") {
+                // device.setNotify(serviceItem.serviceUuid, Global.CHARTIDS, true);
+              }
+            });
+            device.deviceSignalResultStream.listen((result) {
+              print("数据>>$result");
+            });
+          }
+        }).onDone(() {
+          print("连接异常");
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
